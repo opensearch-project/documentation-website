0402d132dd5cf65a59363a315deef0aa4f2b9285
kolchfa-aws <105444904+kolchfa-aws@users.noreply.github.com>
2022-12-19 14:41:42 -0500

    Adds feedback mechanism and more site visual improvements (#2259)
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>
    
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -2,12 +2,31 @@ const feedbackButtons = document.querySelectorAll('.feedback-button');
 
 feedbackButtons.forEach((button) => {
     button.addEventListener('click', function onClick(event) {
-        var url = window.location.href;
-        console.log(url);
-        console.log(button.value);
-        gtag('event', 'feedback', { 
-            'event_category': url, 
-            'event_action': button.value
+        gtag('event', 'feedback_click', { 
+            'helpful': button.value
         });
+        // find the hidden feedback text
+        sibling = button.nextElementSibling;
+
+        while (!(sibling.classList.contains('text-small') && sibling.classList.contains('hidden'))) {
+            sibling = sibling.nextElementSibling;
+        }
+        // show the hidden feedback text
+        if (sibling != null) {
+            sibling.classList.remove('hidden');
+        }
+
+        // disable the feedback buttons
+        button.disabled = true;
+        var buttonSibling;
+        if (button.id === 'yes') {
+            buttonSibling = button.nextElementSibling;
+        }
+        else {
+            buttonSibling = button.previousElementSibling;
+        }
+        if (buttonSibling != null) {
+            buttonSibling.disabled = true;
+        }
     });
 });
