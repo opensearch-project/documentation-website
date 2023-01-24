5bf3f9904cde570826a47a786a681e1805b1daf3
Fanit Kolchina <kolchfa@amazon.com>
2023-01-23 20:46:14 -0500

    Scroll to top when last visit was more than 24 hours ago
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>

--- assets/js/nav-scroll.js
+++ assets/js/nav-scroll.js
@@ -1,12 +1,27 @@
 let siteNav = document.querySelector('.site-nav');
+const key = 'scroll';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const scroll = localStorage.getItem('scroll');
-    if (scroll !== null) {
-        siteNav.scrollTop = parseInt(scroll);
+    const scroll = JSON.parse(localStorage.getItem(key));
+
+    const currentDate = new Date();
+    
+    if (scroll !== null && currentDate.getTime() < scroll.expiry) {
+      siteNav.scrollTop = parseInt(scroll.value);
+    }
+    else {
+      localStorage.removeItem(key);
     }
 });
 
 window.addEventListener('beforeunload', () => {
-  localStorage.setItem('scroll', siteNav.scrollTop);
+  const currentDate = new Date();
+
+  // add the scroll value that expires after one day
+  const scroll = {
+    value: siteNav.scrollTop,
+    expiry: currentDate.getTime() + 24 * 60 * 60 * 1000,
+  }
+  
+  localStorage.setItem(key, JSON.stringify(scroll));
 });
