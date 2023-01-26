b97b48ebcd1bd3e1a41aa56b3dc7610f214bf2de
kolchfa-aws <105444904+kolchfa-aws@users.noreply.github.com>
2023-01-26 11:14:26 -0500

    Scroll to top when last visit was more than 24 hours ago (#2457)
    * Scroll to top when last visit was more than 24 hours ago
    
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>
    
    * Changed localStorage to sessionStorage
    
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>
    
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>
    (cherry picked from commit 8565bb2adfaf630d4997315d0f723b53ad6249b0)

--- assets/js/nav-scroll.js
+++ assets/js/nav-scroll.js
@@ -1,12 +1,27 @@
 let siteNav = document.querySelector('.site-nav');
+const key = 'scroll';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const scroll = localStorage.getItem('scroll');
-    if (scroll !== null) {
-        siteNav.scrollTop = parseInt(scroll);
+    const scroll = JSON.parse(sessionStorage.getItem(key));
+
+    const currentDate = new Date();
+    
+    if (scroll !== null && currentDate.getTime() < scroll.expiry) {
+      siteNav.scrollTop = parseInt(scroll.value);
+    }
+    else {
+      sessionStorage.removeItem(key);
     }
 });
 
 window.addEventListener('beforeunload', () => {
-  localStorage.setItem('scroll', siteNav.scrollTop);
+  const currentDate = new Date();
+
+  // add the scroll value that expires after one day
+  const scroll = {
+    value: siteNav.scrollTop,
+    expiry: currentDate.getTime() + 24 * 60 * 60 * 1000,
+  }
+  
+  sessionStorage.setItem(key, JSON.stringify(scroll));
 });
