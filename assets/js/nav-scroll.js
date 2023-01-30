b71c3371f0385a2210e66122e50d5c81890cab8e
vagimeli <vagimeli@amazon.com>
2023-01-30 15:33:29 -0700

    Revert "Scroll to top when last visit was more than 24 hours ago (#2457)"
    This reverts commit 0a55b3cd3e376998cdf5b8152cbd36026c0e298d.

--- assets/js/nav-scroll.js
+++ assets/js/nav-scroll.js
@@ -1,27 +1,12 @@
 let siteNav = document.querySelector('.site-nav');
-const key = 'scroll';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const scroll = JSON.parse(sessionStorage.getItem(key));
-
-    const currentDate = new Date();
-    
-    if (scroll !== null && currentDate.getTime() < scroll.expiry) {
-      siteNav.scrollTop = parseInt(scroll.value);
-    }
-    else {
-      sessionStorage.removeItem(key);
+    const scroll = localStorage.getItem('scroll');
+    if (scroll !== null) {
+        siteNav.scrollTop = parseInt(scroll);
     }
 });
 
 window.addEventListener('beforeunload', () => {
-  const currentDate = new Date();
-
-  // add the scroll value that expires after one day
-  const scroll = {
-    value: siteNav.scrollTop,
-    expiry: currentDate.getTime() + 24 * 60 * 60 * 1000,
-  }
-  
-  sessionStorage.setItem(key, JSON.stringify(scroll));
+  localStorage.setItem('scroll', siteNav.scrollTop);
 });
