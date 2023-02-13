5b0378e29e8ab6dc9d0fbede891cfaac8d6169d7
Fanit Kolchina <kolchfa@amazon.com>
2023-02-13 16:31:31 -0500

    Added radio buttons, sending comments to GA, and refactored scss
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>

--- assets/js/listener.js
+++ assets/js/listener.js
@@ -7,10 +7,11 @@ document.addEventListener('click', function(event) {
         gtag('event', 'edit_page_click');
     }
     else if (target.matches('.feedback-button')) {
-        sendFeedback(target);
+        // enable the send button
+        document.getElementById('send').disabled = false;
     }
     else if (target.matches('.send-button')) {
-        sendComments(target);
+        sendFeedback(target);
     }
     else if (target.matches('.copy-button')) {
         window.navigator.clipboard.writeText(target.getAttribute('data-text'));
@@ -19,51 +20,51 @@ document.addEventListener('click', function(event) {
 
 document.getElementById("comment").addEventListener("input", function() {
     const text = this.value.trim();
-    if(text.length > 0) {
-      // send text to GA
-      const sendButton = this['nextElementSibling'];
-      sendButton.disabled = false;
-      sendButton['nextElementSibling']?.classList.add('hidden');
-    } 
-    else {
-      this['nextElementSibling'].disabled = true;
+    const sendButton = this['nextElementSibling'];
+    
+    if (!document.getElementById('yes').checked && !document.getElementById('no').checked) {
+        text.length > 0 ? sendButton.disabled = false : sendButton.disabled = true;
     }
-  });
+});
 
 function sendFeedback(button) {
-    gtag('event', 'feedback_click', { 
-        'helpful': button.value
-    });
-    // show the hidden feedback text
-    /*const parent = button.parentElement;
-    const index = [...parent.children].indexOf(button) + 1;
-    const sibling = parent.querySelector(`:nth-child(${index}) ~ .text-small.hidden`);*/
+    // get and send the `helpful` value
+    let helpful = 'none';
+    if (document.getElementById('yes').checked) {
+        helpful = 'yes';
+    }
+    else if (document.getElementById('no').checked) {
+        helpful = 'no';
+    }
 
-    const parent = button.parentElement.parentElement;
-    const index = [...parent.children].indexOf(button.parentElement) + 1;
+    if (helpful !== 'none') {
+        gtag('event', 'feedback_click', { 
+            'helpful': helpful
+        });
+    }
+
+    // get and send the comment
+    const comment = document.getElementById('comment').value.trim();
+    if(comment.length > 0) {
+        gtag('event', 'feedback_comment', { 
+            'comment': comment,
+            'helpful': helpful
+        });
+    }
+
+    // show the hidden feedback text
+    const parent = button.parentElement;
+    const index = [...parent.children].indexOf(button) + 1;
     const sibling = parent.parentElement.querySelector(`:nth-child(${index}) ~ .thank-you`);
     sibling?.classList.remove('hidden');
 
     // disable the feedback buttons
+    document.getElementById('yes').disabled = true;
+    document.getElementById('no').disabled = true;
+
+    // disable the text area
+    document.getElementById('comment').disabled = true;
+
+    // disable the send button
     button.disabled = true;
-    button[button.id === 'yes' ? 'nextElementSibling' : 'previousElementSibling']?.setAttribute('disabled', true);
-}
-
-function sendComments(button) {
-    gtag('event', 'feedback_comment', { 
-        'comment': button.value
-    });
-    // show the hidden feedback text
-    /*const parent = button.parentElement;
-    const index = [...parent.children].indexOf(button) + 1;
-    const sibling = parent.querySelector(`:nth-child(${index}) ~ .text-small.hidden`);*/
-
-    const parent = button.parentElement.parentElement;
-    const index = [...parent.children].indexOf(button.parentElement) + 1;
-    const sibling = parent.parentElement.querySelector(`:nth-child(${index}) ~ .thank-you`);
-    sibling?.classList.remove('hidden');
-
-    // disable the feedback buttons
-    button.disabled = true;
-    button[button.id === 'yes' ? 'nextElementSibling' : 'previousElementSibling']?.setAttribute('disabled', true);
 }
