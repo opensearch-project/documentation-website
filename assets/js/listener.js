f641238359483d35088f05336581f599d56ab63b
Fanit Kolchina <kolchfa@amazon.com>
2023-02-17 15:04:18 -0500

    Addressed code review and added new banner
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>

--- assets/js/listener.js
+++ assets/js/listener.js
@@ -1,3 +1,10 @@
+const yesButton = document.getElementById('yes');
+const noButton = document.getElementById('no');
+const numCharsLabel = document.getElementById('num-chars');
+const sendButton = document.getElementById('send');
+const commentTextArea = document.getElementById('comment');
+const thankYouText = document.getElementById('thank-you');
+
 document.addEventListener('click', function(event) {
     const { target } = event;
     if (target.matches('.feedback-issue')) {
@@ -7,11 +14,10 @@ document.addEventListener('click', function(event) {
         gtag('event', 'edit_page_click');
     }
     else if (target.matches('.feedback-button')) {
-        // enable the send button
-        document.getElementById('send').disabled = false;
+        sendButton.disabled = false;
     }
     else if (target.matches('.send-button')) {
-        sendFeedback(target);
+        sendFeedback();
     }
     else if (target.matches('.copy-button')) {
         window.navigator.clipboard.writeText(target.getAttribute('data-text'));
@@ -23,46 +29,47 @@ document.addEventListener('click', function(event) {
 
 document.getElementById("comment").addEventListener("input", function() {
     const text = this.value.trim();
-    const sendButton = document.getElementById('send');
     
-    if (!document.getElementById('yes').checked && !document.getElementById('no').checked) {
+    if (!yesButton.checked && !noButton.checked) {
         text.length > 0 ? sendButton.disabled = false : sendButton.disabled = true;
     }
 
     // calculate the number of characters remaining
     counter = 350 - this.value.length;
-    document.getElementById('num-chars').innerText = counter + " characters left";
+    numCharsLabel.innerText = counter + " characters left";
 });
 
-function sendFeedback(button) {
+function sendFeedback() {
     let helpful = 'none';
-    if (document.getElementById('yes').checked) {
+    if (yesButton.checked) {
         helpful = 'yes';
     }
-    else if (document.getElementById('no').checked) {
+    else if (noButton.checked) {
         helpful = 'no';
     }
     
-    let comment = document.getElementById('comment').value.trim();
+    let comment = commentTextArea.value.trim();
     if(comment.length == 0) {
         comment = 'none';
     }
 
+    if (helpful === 'none' && comment === 'none') return;
+    
     gtag('event', 'feedback_click', { 
         'helpful': helpful,
         'comment': comment
     });
 
     // show the hidden feedback text
-    document.getElementById('thank-you').classList.remove('hidden');
+    thankYouText.classList.remove('hidden');
 
     // disable the feedback buttons
-    document.getElementById('yes').disabled = true;
-    document.getElementById('no').disabled = true;
+    yesButton.disabled = true;
+    noButton.disabled = true;
 
     // disable the text area
-    document.getElementById('comment').disabled = true;
+    commentTextArea.disabled = true;
 
     // disable the send button
-    button.disabled = true;
+    sendButton.disabled = true;
 }
