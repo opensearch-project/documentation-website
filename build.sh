542650b25934555e2c023669055b67ca43103660
kolchfa-aws <105444904+kolchfa-aws@users.noreply.github.com>
2023-04-04 21:11:35 -0400

    Backport link CI to 2.1 (#3675)
    Signed-off-by: Fanit Kolchina <kolchfa@amazon.com>
    Co-authored-by: Daniel (dB.) Doubrovkine <dblock@amazon.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url
+#!/usr/bin/env bash
+
+JEKYLL_LINK_CHECKER=internal bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url --trace
