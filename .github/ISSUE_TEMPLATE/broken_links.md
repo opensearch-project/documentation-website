b7f07736bbd0f98068768cb8d2ad479efc68f6ac
vagimeli <vagimeli@amazon.com>
2023-04-25 11:58:09 -0600

    Revert "Add CI with link checker. (#3584)"
    This reverts commit 0cebc2d74b95af9a76df38ddfe10a62c9d4c1a2b.

--- .github/ISSUE_TEMPLATE/broken_links.md
+++
@@ -1,7 +0,0 @@
----
-title: '[AUTOCUT] Broken links'
-labels: 'bug'
----
-
-Links checker has failed on push of your commit.
-Please examine the workflow log {{ env.WORKFLOW_URL }}.
--- build.sh
+++ build.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-
-JEKYLL_LINK_CHECKER=internal bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url --trace
+bundle exec jekyll serve --host localhost --port 4000 --incremental --livereload --open-url
