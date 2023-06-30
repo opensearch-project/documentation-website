b34f18dacc8f9e97d251528329fd226f9ad84bf4
Aaron Stephanus <taoist.futility@pm.me>
2023-06-30 16:47:06 -0600

    Adds table of contents scroll and item focus management to the current page or the first toc item
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>

--- assets/js/nav-scroll.js
+++ assets/js/nav-scroll.js
@@ -1,27 +1,30 @@
-let siteNav = document.querySelector('.site-nav');
-const key = 'scroll';
-
 document.addEventListener('DOMContentLoaded', () => {
-    const scroll = JSON.parse(sessionStorage.getItem(key));
-
-    const currentDate = new Date();
-    
-    if (scroll !== null && currentDate.getTime() < scroll.expiry) {
-      siteNav.scrollTop = parseInt(scroll.value);
+    const findParentNavElement = (element) => {
+        if (element === null) {
+            return null;
+        } else if (element.tagName === 'NAV') {
+            return element;
+        } else {
+            return findParentNavElement(element.parentElement);
+        }
+    };
+    const navParent = document.querySelector('body main .side-bar nav#site-nav');
+    if (!navParent) {
+      return;
     }
-    else {
-      sessionStorage.removeItem(key);
+    const activeNavItem = document.querySelector('.nav-list-item.active a.nav-list-link.active');
+    if (activeNavItem) {
+      const activeItemTop = activeNavItem.getBoundingClientRect().y;
+      if (activeItemTop < 0) {
+        navParent.scrollTo(0, activeItemTop);
+        activeNavItem.focus();
+      } else {
+        activeNavItem.focus();
+      }
+    } else {
+      const firstNavItem = navParent.querySelector('ul.nav-list > li.nav-list-item:nth-of-type(1) > a.nav-list-link');
+      if (firstNavItem) {
+        firstNavItem.focus();
+      }
     }
-});
-
-window.addEventListener('beforeunload', () => {
-  const currentDate = new Date();
-
-  // add the scroll value that expires after one day
-  const scroll = {
-    value: siteNav.scrollTop,
-    expiry: currentDate.getTime() + 24 * 60 * 60 * 1000,
-  }
-  
-  sessionStorage.setItem(key, JSON.stringify(scroll));
 });
