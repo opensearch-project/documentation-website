b31a20a99110681cf57eced0f9114e6f54131bd3
Heather Halter <HDHALTER@AMAZON.COM>
2023-11-07 16:22:43 -0800

    Redesigned top nav and footer (#5113) (#5543)
    * Ports over redesigned top banner header navigation, and footer
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds style overrides for search functionality
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds Font Awesome for menu icons
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds style overrides and updated menu item configurations
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds whitespace to fix SASS style rule causing Github action failure
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds dynamic navigation submenu height determination for smooth expand/collapse transitions instead of hardcoded values potentiating effortless change management
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Removes outdated message about old version release
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Corrects padding, positioning, typography for the site footer
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds new footer logo
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Sets footer legal copy right padding on mobile to that of the scroll-to-top button
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adds footer link styles
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adjusts page layout to fit the new container width requirements
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Fixes mobile layout with new header and footer
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Fixes mobile navigation menu errors
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Adjusts padding
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Removes commented out code
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    * Updates top navigation menu items with latest from project site
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    
    ---------
    
    Signed-off-by: Aaron Stephanus <taoist.futility@pm.me>
    Signed-off-by: astephanus <124592019+astephanus@users.noreply.github.com>
    (cherry picked from commit dadb6b1f148ce7438e5d7594fef2cf8b9c75c623)
    
    Co-authored-by: astephanus <124592019+astephanus@users.noreply.github.com>

--- assets/js/header-nav.js
+++ assets/js/header-nav.js
@@ -1,3 +1,5 @@
 document.querySelector('.menu-button')?.addEventListener('click', el => {
-   el.currentTarget.classList.toggle('active');
+   const siteNavigation = document.querySelector('div[role="navigation"].nav-menu-on');
+   siteNavigation?.classList?.toggle?.('active');
+   el.currentTarget.classList?.toggle?.('active');
 });
