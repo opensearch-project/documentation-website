139400f31e844302357da4b7b9c114a8ffbc1d43
Heather Halter <HDHALTER@AMAZON.COM>
2023-11-08 09:13:22 -0800

    Revert "Integrates new header navigation, footer, and layout into the 1.3 branch (#5542)"
    This reverts commit 82e117937a1eb7d9a12718aa21f2e3b66cc66056.

--- assets/js/header-nav.js
+++ assets/js/header-nav.js
@@ -1,5 +1,3 @@
 document.querySelector('.menu-button')?.addEventListener('click', el => {
-   const siteNavigation = document.querySelector('div[role="navigation"].nav-menu-on');
-   siteNavigation?.classList?.toggle?.('active');
-   el.currentTarget.classList?.toggle?.('active');
+   el.currentTarget.classList.toggle('active');
 });
