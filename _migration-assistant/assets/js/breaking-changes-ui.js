06d1e36f8e6bb0c71f87bb1c562b0ef47f268a58
Jugal Chauhan <jugaldc@amazon.com>
2025-08-05 13:59:53 -0500

    remove console log statements
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- _migration-assistant/assets/js/breaking-changes-ui.js
+++ _migration-assistant/assets/js/breaking-changes-ui.js
@@ -49,7 +49,6 @@ const BreakingChangesUI = (function() {
       // Populate dropdowns with all versions
       // Use the global VERSIONS array for compatibility with original code
       const allVersions = VERSIONS;
-      console.log('Available versions:', allVersions);
       this.populateVersionDropdowns(allVersions);
       
       // Set up component checkboxes
@@ -57,7 +56,6 @@ const BreakingChangesUI = (function() {
       
       // Restore original values if they were set (e.g., when navigating back to the page)
       if (originalSourceValue && originalTargetValue) {
-        console.log('Restoring pre-selected values:', originalSourceValue, originalTargetValue);
         elements.sourceSelect.value = originalSourceValue;
         elements.targetSelect.value = originalTargetValue;
       }
@@ -69,7 +67,6 @@ const BreakingChangesUI = (function() {
       // This ensures the DOM has been updated before we check for selected values
       requestAnimationFrame(() => {
         if (elements.sourceSelect.value && elements.targetSelect.value) {
-          console.log('Selected values detected, updating results after DOM update');
           this.updateResults();
         }
       });
@@ -150,7 +147,6 @@ const BreakingChangesUI = (function() {
         // Use the version index to sort in order
         return getVersionIndex(a) - getVersionIndex(b);
       });
-      console.log('Filtered target versions (latest last):', targetVersions);
       
       // Populate target dropdown with filtered versions
       this.populateDropdown(elements.targetSelect, targetVersions, 'Select Target');
@@ -171,7 +167,6 @@ const BreakingChangesUI = (function() {
           // Use the version index to sort in order
           return getVersionIndex(a) - getVersionIndex(b);
         });
-        console.log('Valid targets for', selectedSource, '(latest last):', validTargets);
         this.populateDropdown(elements.targetSelect, validTargets, 'Select Target');
       } else {
         // If no source selected, show all possible targets
@@ -194,7 +189,6 @@ const BreakingChangesUI = (function() {
       if (selectedTarget) {
         // Get valid sources for this target
         const validSources = MIGRATION_MAP.targetToSources[selectedTarget] || [];
-        console.log('Valid sources for', selectedTarget, ':', validSources);
         this.populateDropdown(elements.sourceSelect, validSources, 'Select Source');
       } else {
         // If no target selected, show all possible sources
@@ -214,7 +208,6 @@ const BreakingChangesUI = (function() {
       
       // Get unique components from breaking changes
       const components = this.getUniqueComponents();
-      console.log('Available components:', components);
       
       // Create a checkbox for each component
       components.forEach(component => {
@@ -294,7 +287,6 @@ const BreakingChangesUI = (function() {
       
       // Filter breaking changes directly using the global variables
       const relevantChanges = this.filterBreakingChanges(selectedSource, selectedTarget, selectedComponents);
-      console.log('Filtered breaking changes:', relevantChanges);
       
       // Display results
       this.displayResults(relevantChanges);
